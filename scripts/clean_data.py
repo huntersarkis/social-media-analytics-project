5272324d208178b31c67b21ceb6a3534a42f65be
Hunter Sarkis <hunterjsarkis@gmail.com>
2026-03-06 20:49:40 -0700

    Initial social media engagement analysis project

---
+++ scripts/clean_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load dataset
+df = pd.read_csv("data/raw/social_media_engagement.csv")
+
+# Create engagement column
+df["engagement"] = df["likes"] + df["comments"] + df["shares"]
+
+# Average engagement by platform
+platform_engagement = df.groupby("platform")["engagement"].mean()
+
+# Create bar chart
+platform_engagement.plot(kind="bar")
+
+plt.title("Average Engagement by Platform")
+plt.ylabel("Average Engagement")
+plt.xlabel("Platform")
+
+plt.savefig("visuals/engagement_by_platform.png")
+
+plt.show()
